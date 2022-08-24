5cacec4341364103cfdb1a79229108979bb89cf3
CeCamillo <cesarcamillo2212@gmail.com>
2022-08-24 17:02:52 -0300

    Dependency Injection and Interface

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +10,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MeuPrimeiroController {
 
+    private AtivacaoClienteService ativacaoClienteService;
+
+    public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+        this.ativacaoClienteService = ativacaoClienteService;
+    }
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello(){
+
+        Cliente joao = new Cliente("João", "João@xyz.com", "12997938943");
+        ativacaoClienteService.ativar(joao);
+
         return "Hello!";
     }
 }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,14 +1,20 @@
 package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import com.algaworks.algafood.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-    private NotificadorEmail notificador;
+    private Notificador notificador;
 
+    @Autowired
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+    }
+    
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
