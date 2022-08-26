166b895de3bc9f5960d9f9a91d49a1e6f1f81bf4
CeCamillo <cesarcamillo2212@gmail.com>
2022-08-26 09:54:49 -0300

    Beans Disambiguation

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,19 +1,18 @@
 package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-    private Notificador notificador;
-
+    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
+    private Notificador notificador;
     
     public void ativar(Cliente cliente) {
         cliente.ativar();
