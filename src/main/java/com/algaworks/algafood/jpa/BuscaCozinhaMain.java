6afece0241e4a625e039ad16c93da0eb00dfc484
CeCamillo <cesarcamillo2212@gmail.com>
2022-09-05 12:15:44 -0300

    JPA CRUD operations

--- src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -6,7 +6,9 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusaoCozinhaMain {
+import java.util.List;
+
+public class BuscaCozinhaMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
                 .web(WebApplicationType.NONE)
@@ -14,17 +16,9 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setName("Brasileira");
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setName("Japonesa");
-
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getName());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getName());
+        System.out.println(cozinha.getName());
 
     }
 }
